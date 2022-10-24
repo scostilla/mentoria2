6a463274796fc8c4fb77e57a09f2de5b1e727969
¨scostilla¨ <sebcostilla@gmail.com>
2022-10-24 19:55:44 -0300

    error page

--- src/ErrorNotFound.js
+++ src/ErrorNotFound.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import error_page from './img/error_page.jpg';
-
+import React, { useState } from "react";
+import error_page from "./img/error_page.jpg";
+import "./ErrorNotFound.css";
 
 export function ErrorNotFound() {
-
-    return (
-        <div className='login'>
-            <h1>ERROR 404</h1>
-            <p/>
-            <h1>PAGINA NO ENCONTRADA</h1>
-            <img src={error_page} />
-        </div>
-    )
+  return (
+    <div className="error">
+      <div className="error__container">
+        <h1>ERROR 404</h1>
+        <p />
+        <h1>PAGINA NO ENCONTRADA</h1>
+        <img src={error_page} />
+      </div>
+    </div>
+  );
 }
